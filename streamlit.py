bdcf36b3abcc64b45588a74d36b703f8d49f569f
suehi <110624980+007ekho@users.noreply.github.com>
2024-05-10 23:51:05 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -6,6 +6,20 @@ from read_db import gen_query_response
 from langchain_openai import ChatOpenAI
 from langchain_community.utilities.sql_database import SQLDatabase
 
+user = "EHI007"
+password = "Worker/123"
+warehouse = "COMPUTE_WH"
+role = "ACCOUNTADMIN"
+account = "vwwhgvg-aw51783"
+database="FROSTY_SAMPLE"
+schema="CYBERSYN_FINANCIAL"
+
+from langchain_community.utilities.sql_database import SQLDatabase
+
+snowflake_url = f"snowflake://{user}:{password}@{account}/{database}/{schema}?warehouse={warehouse}&role={role}"
+db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=1,include_tables=['financial_table'])
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, openai_api_key=st.secrets.OPENAI_API_KEY)
+
 st.title("Welcome to your chatbot")
 
 
