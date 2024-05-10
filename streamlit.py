84cacc0fba2165b06a5ef13b08880472ba70fadc
suehi <110624980+007ekho@users.noreply.github.com>
2024-05-10 22:42:01 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,50 +1,6 @@
-# import streamlit as st
-# from read_db import gen_response
-
-# st.title("Welcome to your chatbot")
-
-# if "messages" not in st.session_state.keys():
-#     st.session_state["messages"]=[
-#         {"role":"assistant","content": "Hello there,how can I help you today."}
-#     ]
-
-# #display the messages
-# if "messages" in st.session_state.keys():
-#     for message in st.session_state.messages:
-#         with st.chat_message(message["role"]):
-#             st.write(message["content"])
-
-
-# user_prompt = st.chat_input()
-
-# if user_prompt is not None:
-#     st.session_state.messages.append({
-#         "role":"user",
-#         "content": user_prompt})
-    
-
-#     with st.chat_message("user"):
-#         st.write(user_prompt)
-
-
-# if st.session_state.messages[-1]["role"]!= "assistant":
-#     with st.chat_message("assistant"):
-#         with st.spinner("Thinking...."):
-            
-#             llm_response = gen_response(input)
-            
-#             st.write(llm_response)
-            
-            
-
-
-#     new_ai_message = {"role": "assistant", "content": llm_response}
-#     st.session_state.messages.append(new_ai_message)
-
-
 
 import streamlit as st
-from read_db import gen_response
+from read_db import gen_query
 
 st.title("Welcome to your chatbot")
 
@@ -65,7 +21,7 @@ if user_prompt:
 
     # Generate response if user input is not empty
     with st.spinner("Assistant is thinking..."):
-        llm_response = gen_response(user_prompt)
+        llm_response = gen_query(user_prompt)
         st.write(llm_response)
 
     st.session_state.messages.append({"role": "assistant", "content": llm_response})
