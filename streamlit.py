cc7e9ae4273999966b56c56fa7b72a6f12b92cbc
suehi <110624980+007ekho@users.noreply.github.com>
2024-05-10 23:49:11 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -9,9 +9,9 @@ from langchain_community.utilities.sql_database import SQLDatabase
 st.title("Welcome to your chatbot")
 
 
-snowflake_url = f"snowflake://{user}:{password}@{account}/{database}/{schema}?warehouse={warehouse}&role={role}"
-db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=1,include_tables=['financial_table'])
-llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, openai_api_key=st.secrets.OPENAI_API_KEY)
+# snowflake_url = f"snowflake://{user}:{password}@{account}/{database}/{schema}?warehouse={warehouse}&role={role}"
+# db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=1,include_tables=['financial_table'])
+# llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, openai_api_key=st.secrets.OPENAI_API_KEY)
 
 # Initialize session state
 if "messages" not in st.session_state:
