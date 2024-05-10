db4b0900babfdf0594454965de7716aaa85f3301
suehi <110624980+007ekho@users.noreply.github.com>
2024-05-10 23:48:20 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -3,7 +3,15 @@ import streamlit as st
 from read_db import gen_query
 from read_db import gen_response
 from read_db import gen_query_response
+from langchain_openai import ChatOpenAI
+from langchain_community.utilities.sql_database import SQLDatabase
+
 st.title("Welcome to your chatbot")
+
+
+snowflake_url = f"snowflake://{user}:{password}@{account}/{database}/{schema}?warehouse={warehouse}&role={role}"
+db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=1,include_tables=['financial_table'])
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, openai_api_key=st.secrets.OPENAI_API_KEY)
 
 # Initialize session state
 if "messages" not in st.session_state:
@@ -22,8 +30,8 @@ if user_prompt:
 
     # Generate response if user input is not empty
     with st.spinner("Assistant is thinking..."):
-        query_response = gen_query(user_prompt)
-        llm_response = gen_response(query_response)
+        query_response = gen_query(user_prompt,llm, db)
+        llm_response = gen_response(user_input, llm, db)
         
         st.write(llm_response)
 
