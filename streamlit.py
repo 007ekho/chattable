dfbeb9553e36dcaf159fe1440876644a7036bf7c
suehi <110624980+007ekho@users.noreply.github.com>
2024-05-11 03:31:16 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -20,35 +20,70 @@ snowflake_url = f"snowflake://{user}:{password}@{account}/{database}/{schema}?wa
 db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=1,include_tables=['financial_table'])
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, openai_api_key=st.secrets.OPENAI_API_KEY)
 
-st.title("Welcome to your chatbot")
 
+import streamlit as st
+st.title("Echo Bot")
 
-# snowflake_url = f"snowflake://{user}:{password}@{account}/{database}/{schema}?warehouse={warehouse}&role={role}"
-# db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=1,include_tables=['financial_table'])
-# llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, openai_api_key=st.secrets.OPENAI_API_KEY)
-
-# Initialize session state
+# Initialize chat history
 if "messages" not in st.session_state:
-    st.session_state.messages = [{"role": "assistant", "content": "Hello there, how can I help you today?"}]
+    st.session_state.messages = []
 
-# Display messages
+# Display chat messages from history on app rerun
 for message in st.session_state.messages:
-    with st.container():
-        st.write(f"{message['role'].capitalize()}: {message['content']}")
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-# User input
-user_prompt = st.text_input("You:", "")
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-if user_prompt:
-    st.session_state.messages.append({"role": "user", "content": user_prompt})
-
-    # Generate response if user input is not empty
-    with st.spinner("Assistant is thinking..."):
-        query_response = gen_query(user_prompt)
+    # response = f"Echo: {prompt}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        query_response = gen_query(prompt)
         llm_response = gen_query_response(query_response)
-        llm_res =fin_response(user_prompt,llm_response)
+        response =fin_response(prompt,llm_response)
        
-        st.write(llm_res)
+        # st.write(llm_res)
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
 
-    st.session_state.messages.append({"role": "assistant", "content": llm_response})
+
+
+
+# st.title("Welcome to your chatbot")
+
+
+# # snowflake_url = f"snowflake://{user}:{password}@{account}/{database}/{schema}?warehouse={warehouse}&role={role}"
+# # db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=1,include_tables=['financial_table'])
+# # llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, openai_api_key=st.secrets.OPENAI_API_KEY)
+
+# # Initialize session state
+# if "messages" not in st.session_state:
+#     st.session_state.messages = [{"role": "assistant", "content": "Hello there, how can I help you today?"}]
+
+# # Display messages
+# for message in st.session_state.messages:
+#     with st.container():
+#         st.write(f"{message['role'].capitalize()}: {message['content']}")
+
+# # User input
+# user_prompt = st.text_input("You:", "")
+
+# if user_prompt:
+#     st.session_state.messages.append({"role": "user", "content": user_prompt})
+
+#     # Generate response if user input is not empty
+#     with st.spinner("Assistant is thinking..."):
+#         query_response = gen_query(user_prompt)
+#         llm_response = gen_query_response(query_response)
+#         llm_res =fin_response(user_prompt,llm_response)
+       
+#         st.write(llm_res)
+
+#     st.session_state.messages.append({"role": "assistant", "content": llm_response})
 
