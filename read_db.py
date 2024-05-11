de53301ed5bfb09c00aa9b4868f22cca140e0ba3
suehi <110624980+007ekho@users.noreply.github.com>
2024-05-11 21:35:13 +0100

    Update read_db.py

--- read_db.py
+++ read_db.py
@@ -22,7 +22,7 @@ schema="PUBLIC"
 from langchain_community.utilities.sql_database import SQLDatabase
 
 snowflake_url = f"snowflake://{user}:{password}@{account}/{database}/{schema}?warehouse={warehouse}&role={role}"
-db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=1,include_tables=['BUS_JOURNEY'])
+db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=1,include_tables=['bus_journey'])
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, openai_api_key=st.secrets.OPENAI_API_KEY)
 table_info =db.table_info
 
