e1faa99801407f618b9e570f785a35844ea3c269
suehi <110624980+007ekho@users.noreply.github.com>
2024-05-21 15:41:08 +0100

    Update read_db.py

--- read_db.py
+++ read_db.py
@@ -8,6 +8,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain.chains import LLMChain
+from langchain import hub
 
 
 
